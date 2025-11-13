22c197b56519860ef3bb947a0b391b244421181e
Bielen Pierre <microdev65@gmail.com>
2025-11-13 12:01:09 +0100

    Create utils/algorithms.py with quad remeshing algorithms

---
+++ utils/algorithms.py
@@ -0,0 +1,6 @@
+# Quad Remeshing Algorithms
+
+# Placeholder for quad remeshing algorithms
+# This file will contain the implementation of quad remeshing algorithms.
+
+# Functionality to be added here...
